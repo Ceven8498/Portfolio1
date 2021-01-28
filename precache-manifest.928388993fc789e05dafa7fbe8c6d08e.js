01ba039fd1d61a876602713caea2b5bebe2c17a0
CEven98 <doomfire98@gmail.com>
2021-01-27 23:58:43 -0600

    Updates

---
+++ precache-manifest.928388993fc789e05dafa7fbe8c6d08e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c96d8b523cabc09ceede06e2455ca3",
+    "url": "/Ceven8498/Portfolio1.git/index.html"
+  },
+  {
+    "revision": "142dc14e7dcc54574092",
+    "url": "/Ceven8498/Portfolio1.git/static/css/main.dc3c21be.chunk.css"
+  },
+  {
+    "revision": "05345c28f0bcdc8c0f85",
+    "url": "/Ceven8498/Portfolio1.git/static/js/2.1ae0318d.chunk.js"
+  },
+  {
+    "revision": "a1bd720283e6a590c3ab3794c1f81d89",
+    "url": "/Ceven8498/Portfolio1.git/static/js/2.1ae0318d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142dc14e7dcc54574092",
+    "url": "/Ceven8498/Portfolio1.git/static/js/main.7f6dcb2e.chunk.js"
+  },
+  {
+    "revision": "317af22334611634b289",
+    "url": "/Ceven8498/Portfolio1.git/static/js/runtime-main.cb81681e.js"
+  },
+  {
+    "revision": "ceefafc45a546e52ddfd13cf414296e2",
+    "url": "/Ceven8498/Portfolio1.git/static/media/MERN-logo.ceefafc4.png"
+  },
+  {
+    "revision": "d27d6d7bda81b364e1a19664e7ee1b10",
+    "url": "/Ceven8498/Portfolio1.git/static/media/RecipeGen.d27d6d7b.png"
+  },
+  {
+    "revision": "96f038b128363833d10d6745f827b236",
+    "url": "/Ceven8498/Portfolio1.git/static/media/TPF.96f038b1.gif"
+  },
+  {
+    "revision": "27f37cee2ab817ef72af920b562dac82",
+    "url": "/Ceven8498/Portfolio1.git/static/media/note-taker.27f37cee.png"
+  },
+  {
+    "revision": "17b29196a5fb31e39d52838ea8b0a09d",
+    "url": "/Ceven8498/Portfolio1.git/static/media/red_bag.17b29196.png"
+  },
+  {
+    "revision": "842c50644d4b7480fef8aa4f682a1376",
+    "url": "/Ceven8498/Portfolio1.git/static/media/resume.842c5064.jpg"
+  },
+  {
+    "revision": "25fae8f053265d481b97856be7872125",
+    "url": "/Ceven8498/Portfolio1.git/static/media/work-scheduler.25fae8f0.png"
+  }
+]);
